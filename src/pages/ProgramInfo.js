a0833b14ef77be75f2e313e759ecc728fb609373
배성윤 <129399109+Bae309@users.noreply.github.com>
2024-11-27 00:43:46 +0900

    Add files via upload

---
+++ src/pages/ProgramInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProgramInfo = () => {
+    return (
+        <main>
+            <h2>프로그램 안내</h2>
+            <p>스포츠 강좌 및 프로그램에 대한 안내를 제공합니다.</p>
+        </main>
+    );
+};
+
+export default ProgramInfo;
